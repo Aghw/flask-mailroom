c0c7f853f2bcbd6b7ae92de768f44c420f259757
Aghw <sam.asmerom@outlook.com>
2020-09-27 02:23:01 -0700

    completed the form to allow user to enter donor-name and donation-amount

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from model import Donation 
 
 app = Flask(__name__)
+app.secret_key = b'\x11\x14\x17\x8a\xf8@\x03\xf0\xf9U[\x93"\xfb\x94\x00\x7fGN\xad\x8a~\xb3\x8f'
+
 
 @app.route('/')
 def home():
@@ -24,7 +26,36 @@ def all():
 
 @app.route('/donate', methods=['GET', 'POST'])
 def donate():
-    return render_template('donate.jinja2')
+    # session['donation_amount']
+
+    if 'donation_amount' not in session:
+        session['donation_amount'] = 0
+    
+    #session['donor_name']
+    if 'donor_name' not in session:
+        session['donor_name'] = 'XXXX'
+
+    if request.method == 'POST':
+        amount = int(request.form['amount'])
+        fullname = request.form['fullname']
+        session['donation_amount'] = amount
+        session['donor_name'] = fullname
+
+    return render_template('donate.jinja2', session=session)
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    donate()
+    code = base64.b32encode(os.urandom(8)).decode().strip("=")
+    
+    # amount = session.get('donation_amount', 0)
+    amount = session.get('donation_amount', 0)
+
+    save_donation = Donation(value=amount, donor_id=2)
+    save_donation.save()
+
+    return render_template('save.jinja2', code=code)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
